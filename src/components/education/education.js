f021c775282e8083aa71cb3716ea5fa17edeb55a
Aditya Gupta <aditya.gupta_chd@yahoo.ca>
2023-06-25 15:06:14 -0700

    Added separate components of dashboard + css

---
+++ src/components/education/education.js
@@ -0,0 +1,7 @@
+import classes from './education.module.css';
+
+function Education() {
+  return <div className={classes.main}>Education</div>;
+}
+
+export default Education;
--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,9 +1,19 @@
-
+import classes from "./dashboard.module.css";
+import Introduction from "../../components/introduction/introduction";
+import Education from "../../components/education/education";
+import Interests from "../../components/interests/interests";
 
 function DashboardPage() {
-  return <div>
-
-  </div>;
+  return (
+    <div className={classes.dashboard}>
+      <div className={classes.greeting}>
+        <h1>Hi there</h1>
+      </div>
+      <Introduction />
+      <Education />
+      <Interests />
+    </div>
+  );
 }
 
 export default DashboardPage;
