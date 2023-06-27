89130803c9295011fe1ab82267ba14f889912531
Aditya Gupta <aditya.gupta_chd@yahoo.ca>
2023-06-27 13:01:46 -0700

    Stylized introduction section

--- src/components/introduction/introduction.js
+++ src/components/introduction/introduction.js
@@ -1,7 +1,19 @@
 import classes from "./introduction.module.css";
+import { SECTION_INTRODUCTION } from "../../strings";
 
 function Introduction() {
-  return <div className={classes.main}>Introduction</div>;
+  return (
+    <div className={classes.introduction}>
+      <div className={classes.content}>
+        <div className={classes.introductionText}>
+          <h1>{SECTION_INTRODUCTION.GREETING}</h1>
+          <p>{SECTION_INTRODUCTION.ABOUT_ME}</p>
+          <p>{SECTION_INTRODUCTION.CURRENTLY}</p>
+        </div>
+        <img src="/assets/images/ProfilePicture.jpeg" alt="Profile headshot" />
+      </div>
+    </div>
+  );
 }
 
 export default Introduction;
