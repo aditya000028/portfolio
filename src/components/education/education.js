346ec893ea03d2d68c61fae3b45dc502d6f19042
Aditya Gupta <aditya.gupta_chd@yahoo.ca>
2023-06-29 12:21:57 -0700

    Added content to education section and styles

--- src/components/education/education.js
+++ src/components/education/education.js
@@ -1,7 +1,20 @@
-import classes from './education.module.css';
+import classes from "./education.module.css";
+
+import { SECTION_EDUCATION } from '../../strings';
 
 function Education() {
-  return <div className={classes.main}>Education</div>;
+  return (
+    <div className={classes.education}>
+      <div className={classes.content}>
+        <img src="/assets/images/SFU-logo.png" alt="SFU" />
+        <div className={classes.educationText}>
+          <h1>{SECTION_EDUCATION.HEADER}</h1>
+          <p>{SECTION_EDUCATION.DEGREE_TEXT}</p>
+          <p>{SECTION_EDUCATION.COURSES_TEXT}</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Education;
--- src/strings.js
+++ src/strings.js
@@ -10,4 +10,8 @@ export const SECTION_INTRODUCTION = {
   CURRENTLY: "My main focus is web and software development, and I am very comfortable in both the front and back end. I have gained a year of internship experience, working for Visier and PNI Digital Media as a Full Stack Developer. Currently, I am looking for a full-time role with an organization to jumpstart my career with. "
 }
 
-// ... more
+export const SECTION_EDUCATION = {
+  HEADER: "My Education",
+  DEGREE_TEXT: "I am an April 2023 graduate with a Bachelors of Computing Science from Simon Fraser University (SFU) in Burnaby, Canada. Additionally, I achieved a Distinction in Co-op. This means that the school has recognized that I have done 1+ year of internship work with organizations. I have also been a two-time recipient of the Dean's Honor Roll award, which signifies that I achieved a grade point average of above 3.5 in a full course load semester.",
+  COURSES_TEXT: "Some completed courses which I would like to highlight are: Web Systems Architecture, Database Systems I & II, Object Oriented Design in Java, and Distributed Systems.",
+}
