eea16c973eaf01a45460bc9127fadaf88a666869
Aditya Gupta <aditya.gupta_chd@yahoo.ca>
2023-07-03 14:32:04 -0700

    Made image into background image

--- src/components/education/education.js
+++ src/components/education/education.js
@@ -1,4 +1,4 @@
-import DownloadButton from '../download-button/downloadButton';
+import DownloadButton from "../download-button/downloadButton";
 import classes from "./education.module.css";
 
 import { SECTION_EDUCATION, MISC } from "../../strings";
@@ -7,15 +7,16 @@ function Education() {
   return (
     <div className={classes.education}>
       <div className={classes.content}>
-        <img src="/assets/images/SFU-logo.png" alt="SFU" />
-        <div className={classes.contentInfo}>
-          <div className={classes.educationText}>
-            <h1>{SECTION_EDUCATION.HEADER}</h1>
-            <p>{SECTION_EDUCATION.DEGREE_TEXT}</p>
-            <p>{SECTION_EDUCATION.COURSES_TEXT}</p>
-          </div>
-          <DownloadButton buttonText={MISC.DOWNLOAD_TRANSCRIPT} fileToDownload={MISC.TRANSCRIPT_NAME} newFileName={MISC.NEW_TRANSCRIPT_NAME} />
+        <div className={classes.educationText}>
+          <h1>{SECTION_EDUCATION.HEADER}</h1>
+          <p>{SECTION_EDUCATION.DEGREE_TEXT}</p>
+          <p>{SECTION_EDUCATION.COURSES_TEXT}</p>
         </div>
+        <DownloadButton
+          buttonText={MISC.DOWNLOAD_TRANSCRIPT}
+          fileToDownload={MISC.TRANSCRIPT_NAME}
+          newFileName={MISC.NEW_TRANSCRIPT_NAME}
+        />
       </div>
     </div>
   );
