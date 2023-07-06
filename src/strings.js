9c249013b25920dc64c1de248374ee416994dc93
Aditya Gupta <aditya.gupta_chd@yahoo.ca>
2023-07-05 20:49:30 -0700

    Refactored work experience description and strings

--- src/strings.js
+++ src/strings.js
@@ -1,20 +1,24 @@
 export const HOME_GREETING = {
   GREETING: "Hi there.",
   HEADLINE: "I'm Aditya Gupta - a Full Stack Developer.",
-  WELCOME: "Welcome to my portfolio!"
-}
+  WELCOME: "Welcome to my portfolio!",
+};
 
 export const HOME_SECTION_INTRODUCTION = {
   GREETING: "Who am I?",
-  ABOUT_ME: "I am Aditya Gupta, a Full Stack Developer. Growing up, I did not want to work with computers, and found them intimidating. But I took a data structures and algorithms course in my first year of university course and fell in love with coding. Fast-forward to today, and here I am coding as my passion.",
-  CURRENTLY: "My main focus is web and software development, and I am very comfortable in both the front and back end. I have gained a year of internship experience, working for Visier and PNI Digital Media as a Full Stack Developer. Currently, I am looking for a full-time role with an organization to jumpstart my career with. "
-}
+  ABOUT_ME:
+    "I am Aditya Gupta, a Full Stack Developer. Growing up, I did not want to work with computers, and found them intimidating. But I took a data structures and algorithms course in my first year of university course and fell in love with coding. Fast-forward to today, and here I am coding as my passion.",
+  CURRENTLY:
+    "My main focus is web and software development, and I am very comfortable in both the front and back end. I have gained a year of internship experience, working for Visier and PNI Digital Media as a Full Stack Developer. Currently, I am looking for a full-time role with an organization to jumpstart my career with. ",
+};
 
 export const HOME_SECTION_EDUCATION = {
   HEADER: "My Education",
-  DEGREE_TEXT: "I am an April 2023 graduate with a Bachelors of Computing Science from Simon Fraser University (SFU) in Burnaby, Canada. Additionally, I achieved a Distinction in Co-op. This means that the school has recognized that I have done 1+ year of internship work with organizations. I have also been a two-time recipient of the Dean's Honor Roll award, which signifies that I achieved a grade point average of above 3.5 in a full course load semester.",
-  COURSES_TEXT: "Some completed courses which I would like to highlight are: Web Systems Architecture, Database Systems I & II, Object Oriented Design in Java, and Distributed Systems.",
-}
+  DEGREE_TEXT:
+    "I am an April 2023 graduate with a Bachelors of Computing Science from Simon Fraser University (SFU) in Burnaby, Canada. Additionally, I achieved a Distinction in Co-op. This means that the school has recognized that I have done 1+ year of internship work with organizations. I have also been a two-time recipient of the Dean's Honor Roll award, which signifies that I achieved a grade point average of above 3.5 in a full course load semester.",
+  COURSES_TEXT:
+    "Some completed courses which I would like to highlight are: Web Systems Architecture, Database Systems I & II, Object Oriented Design in Java, and Distributed Systems.",
+};
 
 export const MISC = {
   DOWNLOADING: "Downloading",
@@ -24,8 +28,8 @@ export const MISC = {
   TRANSCRIPT_NAME: "SFU Final Transcript.pdf",
   NEW_RESUME_NAME: "Aditya Gupta - Resume",
   NEW_TRANSCRIPT_NAME: "Aditya Gupta - SFU Transcript",
-  WORK_IN_PROGRESS: "This is a work in progress"
-}
+  WORK_IN_PROGRESS: "This is a work in progress",
+};
 
 export const CONTACT_ME = {
   LINKEDIN: "LinkedIn",
@@ -33,20 +37,40 @@ export const CONTACT_ME = {
   GITHUB: "Github",
   GITHUB_LINK: "https://github.com/aditya000028",
   EMAIL: "aditya.gupta_chd@yahoo.ca",
-  PHONE: "+1 (425)-524-3452"
-}
+  PHONE: "+1 (425)-524-3452",
+};
 
 export const WORK_EXPERIENCE_GREETING = {
   WORK: "Work",
   EXPERIENCE: "Experience",
-  DESCRIPTION: "I have completed two internships both as a Full Stack Developer, lasting a total of a year. My first internship was with PNI Digital Media from January - April 2020. Initially commiting to a 12 month long internship, it was unfortunately cut short due to COVID-19. My second internship was with Visier from January - August 2022. I have learned quite a bit from both of these internships, especially growing in front-end development."
-}
+  DESCRIPTION:
+    "I have completed two internships both as a Full Stack Developer, lasting a total of a year. My first internship was with PNI Digital Media from January - April 2020. Initially commiting to a 12 month long internship, it was unfortunately cut short due to COVID-19. My second internship was with Visier from January - August 2022. I have learned quite a bit from both of these internships, especially growing in front-end development.",
+};
 
 export const WORK_EXPERIENCE_SECTION_VISIER = {
   NAME: "Visier",
   ROLE: "Software Developer Intern",
-  DESCRIPTION: "",
-  SKILLS: ['Skill 1', 'Skill 1', 'Skill 1', 'Skill 1', 'Skill 1', 'Skill 1', 'Skill 1'],
+  DESCRIPTIONS: [
+    "Being my second internship, I felt comfortable in my skills and was confident I was going to do well. But when the internship started and my manager asked me what I was looking to work on, I reflected on myself and realized that I always had a weakness - front-end developement. I had always shyed away from it and found it incredibly frustrating. So knowing this I replied that I would love to work on front-end work, and truly develop those skills",
+    "By the end of the internship, I was able to complete many front-end features using Angular, Typescript, HTML, and CSS. This included creating a unique data structure to store 1000+ pieces of data. I also co-led development work on a different major feature, where I was responsible for doing front-end development. I regularly trained my interpersonal skills by communicating with various teams through Slack to satisfy requirements. I even got the opportunity to present some of my work to the company! And although I focused more on front-end development, I still got the chance to complete back-end tickets using Java, Scala, DBeaver, and Postman.",
+    "Finally, I also took part in a company wide dev-day, which is a day to work on new things with new people. Teaming up with 2 other interns and a staff engineer, our team was able to finish in second place!",
+    "Overall, I learned quite a bit from this internship, especially in front-end developement. I also improved my interpersonal skills by communicating with various teams and members regularly. Finally, I also gained experience in learning industry level backend development."
+  ],
+  SKILLS: [
+    "Data Structures",
+    "Angular",
+    "Typescript",
+    "HTML",
+    "CSS",
+    "RxJS",
+    "Java",
+    "Scala",
+    "Postman",
+    "DBeaver",
+    "Jira",
+    "Git",
+    "Slack",
+  ],
   IMAGE_PATH: "/assets/images/Visier-logo.jpg",
-  IMAGE_ALT: "Visier Logo"
-}
+  IMAGE_ALT: "Visier Logo",
+};
